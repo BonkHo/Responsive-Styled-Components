c3d8ad5fa866f22cd0740d5f80b36961601e5a17
John <John@DESKTOP-SRO1BKN>
2021-07-10 09:34:59 -0700

    Initial Push

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import React from "react";
+import NavBar from "./components/NavBar";
+import { BrowserRouter as Router } from "react-router-dom";
+
+function App() {
+    return (
+        <Router>
+            <NavBar />
+        </Router>
+    );
+}
+
+export default App;
